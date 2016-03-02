891ca7a225785b572cfdf8285f68bb224122beca
FSEVERI\gutu2891 <FSEVERI\gutu2891@lmatbi-17.fseveri.it>
2016-03-02 13:03:24 +0100

    Added packages

---
+++ src/java/it/severi/gdtrsuper/config/Config.java
@@ -0,0 +1,38 @@
+package it.severi.gdtrsuper.config;
+
+import javax.websocket.ClientEndpointConfig.Configurator;
+import org.springframework.context.annotation.*;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.JstlView;
+import org.springframework.web.servlet.view.UrlBasedViewResolver;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author FSEVERI\gutu2891
+ */
+@Configuration
+@ComponentScan(value="it.severi.gdtrsuper")
+@EnableWebMvc
+public class Config extends WebMvcConfigurerAdapter{
+    
+ 
+    @Bean  
+    public UrlBasedViewResolver setupViewResolver() {  
+        UrlBasedViewResolver resolver = new UrlBasedViewResolver();  
+        resolver.setPrefix("/WEB-INF/jsp/");  
+        resolver.setSuffix(".jsp");  
+        resolver.setViewClass(JstlView.class);  
+        return resolver;  
+    }  
+     @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**").addResourceLocations("/WEB-INF/resources/*");
+    } 
+}
---
+++ src/java/it/severi/gdtrsuper/config/HibernateUtil.java
@@ -0,0 +1,37 @@
+package it.severi.gdtrsuper.config;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import it.severi.gdtrsuper.db.*;
+import org.hibernate.cfg.AnnotationConfiguration;
+import org.hibernate.SessionFactory;
+
+/**
+ * Hibernate Utility class with a convenient method to get Session Factory
+ * object.
+ *
+ * @author Cristy
+ */
+public class HibernateUtil {
+
+    public static final SessionFactory sessionFactory;
+    
+    static {
+        try {
+            // Create the SessionFactory from standard (hibernate.cfg.xml) 
+            // config file.
+            sessionFactory = new AnnotationConfiguration().configure().addPackage("it.severi.gdtrsuper.db").addAnnotatedClass(Artista.class).addAnnotatedClass(Categoria.class).addAnnotatedClass(Commento.class).addAnnotatedClass(Evento.class).addAnnotatedClass(Utente.class).buildSessionFactory();
+        } catch (Throwable ex) {
+            // Log the exception. 
+            System.err.println("Initial SessionFactory creation failed." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+    
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+}
