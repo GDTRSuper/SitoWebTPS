79713d69d18cb73a92d1d1d2eec2be402d57b097
FSEVERI\trovo2987 <FSEVERI\trovo2987@linf2-04.fseveri.it>
2016-03-12 09:32:06 +0100

    rating js

--- web/WEB-INF/res/js/rating-comment.js
+++ web/WEB-INF/res/js/rating-comment.js
@@ -1,8 +1,14 @@
 $(document).ready(function() {
-    $('.ratings_star').click(function() {
-       $('.ratings_star').removeClass('glyphicon glyphicon-star-empty'); // Removes the selected class from all of them
-       $(this).addClass('glyphicon glyphicon-star'); // Adds the selected class to just the one you clicked
-
+    $('.ratings_star').hover(function() {
+        var i=1;
+        $('.glyphicon-star').each(function(){
+            $(this).removeClass('glyphicon-star').addClass('glyphicon-star-empty');
+        });
+        var rating = $(this).data('rating'); // Get the rating from the selected star
+        while(i<=rating){
+            $('#star'+i).removeClass('glyphicon-star-empty').addClass('glyphicon-star'); // Adds the selected class to just the one you clicked
+            i++;
+        }
        var rating = $(this).data('rating'); // Get the rating from the selected star
        $('#rating').val(rating); // Set the value of the hidden rating form element
     });  
