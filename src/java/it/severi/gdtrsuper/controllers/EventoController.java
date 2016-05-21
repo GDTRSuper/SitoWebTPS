e44ccccfe0febf6a6cdfbfe5b64accafca62e1a8
FSEVERI\trovo2987 <FSEVERI\trovo2987@linf1-28.fseveri.it>
2016-05-21 12:00:45 +0200

    eventi creati

--- src/java/it/severi/gdtrsuper/controllers/EventoController.java
+++ src/java/it/severi/gdtrsuper/controllers/EventoController.java
@@ -5,25 +5,30 @@
  */
 package it.severi.gdtrsuper.controllers;
 
+import it.severi.gdtrsuper.db.Artista;
 import it.severi.gdtrsuper.db.Commento;
 import it.severi.gdtrsuper.db.Evento;
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
 import po.ManageDatabase;
 
-/**
- *
- * @author Giacomo
- */
+
 @Controller
+@SessionAttributes({"utente"})
 public class EventoController {
   ManageDatabase db = new ManageDatabase();
     
     @RequestMapping(value="/evento",method=RequestMethod.GET)
-    public String index(ModelMap map, @RequestParam(value="id", required=true)int id){
+    public String index(ModelMap map, @RequestParam(value="id", required=false)int id,@ModelAttribute Evento ev){
         map.put("evento", db.getEventoById(id));
         return "evento";
     }
@@ -41,5 +46,36 @@ public class EventoController {
         // db.aggiornaEvento(evento);
         map.put("evento",evento);
         return "aggiungiCommento";
+    }
+    @RequestMapping(value="/createEvento",method=RequestMethod.GET)
+    public String aggiugniEvento(ModelMap map){
+        map.put("categorie", db.getCategorie());
+        map.put("artisti",db.getArtisti());
+        return "nuovoEvento";
+    }
+    @RequestMapping(value="/aggiungiEvento",method=RequestMethod.GET)
+    public String aggiugniEvento(ModelMap map,/*@ModelAttribute("utente") Utente user,*/ @RequestParam(value="titolo", required=true) String titolo, @RequestParam(value="data", required=true) Date data, @RequestParam(value="luogo", required=true) String luogo, @RequestParam(value="descrizione", required=true)String descr, @RequestParam(value="immagine", required=true)String img, @RequestParam(value="categoria", required=true)int cat,@RequestParam(value="artisti", required=false)ArrayList<Integer> artisti){
+        Evento evento = new Evento();
+        evento.setCategoria(db.getCategoriaByID(cat));
+        evento.setTitolo(titolo);
+        evento.setData(data);
+        evento.setLuogo(luogo);
+        evento.setCreatore(db.getUtenteByNick("bruno"));
+        evento.setDescrizione(descr);
+        evento.setImmagine(img);
+        Collection<Artista> arts = new ArrayList<>();
+        if(artisti!=null){
+            for(Integer a: artisti){
+                arts.add(db.getArtistaByID(a));
+            }
+        }
+        evento.setArtistiCollection(arts);
+        //evento.setCreatore(user);
+        db.aggiornaEvento(evento);
+        System.out.println("evento: "+evento);
+        List<Evento> v = db.cercaEvento(titolo);
+        Evento ev = v.get(0);
+        int i = ev.getId();
+        return "redirect:/evento?id="+i;
     }
 }
